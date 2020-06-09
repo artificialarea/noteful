6029cf8aab26e7d18919d9ffd0b4bd8dd3ad4101
Sacha Sedriks <sachased@gmail.com>
2020-06-08 22:26:37 -0700

    added some boilerplate components to complete the set and updated all components from function to class for consistency sake.

---
+++ src/NotePageMain/NotePageMain.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './NotePageMain.css'
+
+
+export default class NotePageMain extends React.Component {
+  render() {
+    return (
+      <div />
+    )
+  }
+}
---
+++ src/NotePageMain/NotePageMain.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import NotePageMain from './NotePageMain';
+
+describe('<NotePageMain />', () => {
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div')
+    ReactDOM.render(<NotePageMain />, div)
+    ReactDOM.unmountComponentAtNode(div)
+  })
+
+})
