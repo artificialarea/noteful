029356d8fd8b03f5380dd3467f4dc16df9661d34
Sacha Sedriks <sachased@gmail.com>
2020-06-09 17:19:43 -0700

    updated NoteListNav to generate 'selected' class name for selected folder.id.

--- src/NoteListNav/NoteListNav.js
+++ src/NoteListNav/NoteListNav.js
@@ -6,15 +6,25 @@ import './NoteListNav.css';
 export default class NoteListNav extends React.Component {
 
   render() {
-    const { folders } = this.props;    
+    const { folders, selected } = this.props;    
     return (
       <>
         <ul>
-          {folders.map(folder => 
-            <li key={folder.id}>
+          {folders.map(folder => {
+
+            /* give each folder in the array a class name 
+            *plus* the class name 'selected' if the folder's id matches the selected props (props.match.params.folderId)
+            via this crafty conditional ternary operator */
+            const classes = selected === folder.id 
+              ? 'folder selected'
+              : 'folder'
+
+            return (
+            <li className={classes} key={folder.id}>
               <Link to={`/folders/${folder.id}`}>{folder.name}</Link>
             </li>
-          )}
+            )
+          })}
         </ul>
         <button>Add folder</button>
       </>
