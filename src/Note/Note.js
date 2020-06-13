f27e1302e31e1be6e4eb8597bfe812b2388e4e0b
Sacha Sedriks <sachased@gmail.com>
2020-06-13 13:01:26 -0700

    updated Last modified timestamp so human readable.

--- src/Note/Note.js
+++ src/Note/Note.js
@@ -2,9 +2,47 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Note.css';
 
+// format Date into something human readable
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/Date
+
+// Solution via Blake via this on stack overflow: https://stackoverflow.com/questions/3552461/how-to-format-a-javascript-date
+function formatDate(date) {
+  var monthNames = [
+    "Jan", "Feb", "Mar",
+    "Apr", "May", "Jun", "Jul",
+    "Aug", "Sep", "Oct",
+    "Nov", "Dec"
+  ];
+
+  var day = date.getDate();
+  var monthIndex = date.getMonth();
+  var year = date.getFullYear();
+
+  return monthNames[monthIndex] + ' ' + day + ', ' + year;
+}
+
+function formatTime(date) {
+  
+  var hours = date.getHours();
+  var mins = date.getMinutes();
+  var secs = date.getSeconds();
+  if (mins < 10) {
+    mins = '0' + mins;
+  }
+  if (secs < 10) {
+    secs = '0' + secs;
+  }
+
+  return hours + ':' + mins + ':' + secs;
+}
 
 export default class Note extends React.Component {
   render() {
+    // const modified = new Date(this.props.modified);
+    const modified = formatDate(new Date(this.props.modified));
+    const modifiedTime = formatTime(new Date(this.props.modified));
+    
+    console.log('modified: ', modified)
     return (
       <li 
         key={this.props.id}
@@ -12,7 +50,7 @@ export default class Note extends React.Component {
       >
         <h2><Link to={`/notes/${this.props.id}`}>{this.props.name}</Link></h2>
         <div>
-          <p>Date modified on {this.props.modified}</p>
+          <p>Last modified: <span className="date">{modified}</span> <span className="time">{modifiedTime}</span></p>
           <button>Delete Note</button> 
         </div>
       </li>
